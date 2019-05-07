4a1177ab964e1cf602a75cfa37e7e4c2c9a99844
lirunquan <1617277400@qq.com>
2019-05-07 09:00:08 +0800

    make.py

---
+++ Ex_3/make.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+
+iris = datasets.load_iris()
+def predicted_result():
+	x_train,x_test,y_train,y_test = train_test_split(iris.data,iris.target,test_size=0.33,random_state=45)
+	ss = StandardScaler()
+	x_train = ss.fit_transform(x_train)
+	x_test = ss.transform(x_test)
+	knn = KNeighborsClassifier(n_neighbors=5)	
+	knn.fit(x_train, y_train)
+	y_predict = knn.predict(x_test)
+	label = iris.target_names
+	for i in range(len(y_predict)):
+		print("Test %d: truth[%s]\tpredicted[%s]"%((i+1),label[y_predict[i]],label[y_test[i]]))
+	print("Accuracy with KNN: ", knn.score(x_test,y_test))
+def cross():
+	x_train,x_test,y_train,y_test = train_test_split(iris.data,iris.target,test_size=0.33,random_state=45)
+	params = {'n_neighbors': [2,3,4,5]}
+	knn = KNeighborsClassifier()	
+	gcv = GridSearchCV(knn, param_grid=params, cv=5)
+	gcv.fit(x_train, y_train)
+	print("Cross Validation Accuracy: ",gcv.score(x_test, y_test))
+	print("Best Result in Cross Validation: ",gcv.best_score_)
+	print("Best estimator: ",gcv.best_estimator_)
+
+if __name__ == '__main__':
+	#pass
+	predicted_result()
+	cross()
